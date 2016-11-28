6774539b0725ff93c39f08adbccdb36cc53e43f9
Nate Rush <narush@wharton.upenn.edu>
2016-11-27 23:58:33 -0500

    Update code.sol
    Bug fixes from Reddit, with exception of Leap Year Issue

--- code.sol
+++ code.sol
@@ -1,5 +1,7 @@
 pragma solidity ^0.4.2;
 
+//GiftOfFriendship will only be mortal while it is in beta. 
+//Otherwise, the owner could take the money at any point. 
 contract Mortal {
     address owner;
 
@@ -9,19 +11,24 @@ contract Mortal {
 }
 
 contract GiftOfFriendship is Mortal {
-    public address friend;
-    public uint ownersBirthday;
-    public uint friendsBirthday;
-    public uint256 totalPresentValue;
+    address public friend;
+    uint public ownersBirthday;
+    uint public friendsBirthday;
+    uint256 public totalPresentValue;
+    uint256 public minPresentValue;
 
     // Constructor
-    function GiftOfFriendship(uint _ownersBirthday, uint _friendsBirthday) {
+    function GiftOfFriendship(uint _ownersBirthday, uint _friendsBirthday, uint256 _minPresentValue) {
         ownersBirthday = _ownersBirthday;
         friendsBirthday = _friendsBirthday;
+        minPresentValue = _minPresentValue;
     }
 
+    //this function is outside of contructor because this contract is meant
+    //to get one's friends into ethereum. Therefore, they are unlikely to
+    //already have an account. 
     function makeAFriend(address _friend) {
-        if (friend == 0)
+        if (friend != 0)
             throw;
         if (msg.sender != owner)
             throw;
@@ -29,7 +36,10 @@ contract GiftOfFriendship is Mortal {
     }
 
     // make sure you send some ether along with this as a present
-    function happyBirthday() isNotCallersBirthday() {
+    function happyBirthday() isFriendsBirthday payable {
+        //less than so can make the minPresentValue 0 and it woln't always throw
+        if (msg.value < minPresentValue)
+            throw;
 
         if (msg.sender == owner) {
             friendsBirthday = block.timestamp + 365 days;
@@ -43,11 +53,14 @@ contract GiftOfFriendship is Mortal {
     }
 
     //this throws if its not within 48 hours of a birthday
-    modifier isNotCallersBirthday() {
-
+    modifier isFriendsBirthday() {
+        if (msg.sender != owner || msg.sender != friend) 
+            throw;
+            
         int currentTime = int (block.timestamp);
-        int distanceFromOthersBirthday = 2 days;
-
+        //like initializing to highest value in sorting algorithm 
+        int distanceFromOthersBirthday = 10 days;
+        
         if (msg.sender == owner) {
             distanceFromOthersBirthday = currentTime - (int) (friendsBirthday);
 
@@ -55,12 +68,11 @@ contract GiftOfFriendship is Mortal {
             distanceFromOthersBirthday = currentTime - (int) (ownersBirthday);
 
         }
-        if (distanceFromOthersBirthday > 24 hours || distanceFromOthersBirthday > -24 hours) {
+        if (distanceFromOthersBirthday > 24 hours || distanceFromOthersBirthday < -24 hours) {
             throw;
         }
         _;
     }
-
 
     function takeMyPresent() {
         if (msg.sender == owner) {
